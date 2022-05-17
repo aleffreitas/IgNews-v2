7bad5c578b9d637912b27bfeba99c8e072f4a1b2
Alef Freitas <alef.freitas07@gmail.com>
2022-05-17 10:16:55 -0300

    chore: added integration with faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,36 @@
+import { query } from 'faunadb';
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
 
   callbacks: {
     session({ session, token, user }) {
       return session
+    },
+
+    async signIn({user, account, profile,  credentials}){
+      const isAllowedToSignIn = true;
+      const { email } = user;
+
+      if(isAllowedToSignIn){
+        await fauna.query(
+          query.Create(
+            query.Collection("users"),
+            { data: { email }}
+          )
+        )
+  
+        return true
+
+      } else {
+        return false
+      }
+
     }
+
   },
 
   providers: [
