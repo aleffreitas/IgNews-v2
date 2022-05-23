b058c6aac8e24bffcfa40394f5e180351d19ac03
Alef Freitas <alef.freitas07@gmail.com>
2022-05-23 09:47:31 -0300

    chore: finished webhooks

--- src/pages/api/auth/webhooks.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export default (request: NextApiRequest, response: NextApiResponse) => {
-  console.log('evento recebido')
-
-  response.status(200).json({ message: 'ok' })
-}
