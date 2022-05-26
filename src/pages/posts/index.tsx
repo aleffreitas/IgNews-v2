bfb6e0e2bc58c9d2585de5e2628a3da06db3a847
Alef Freitas <alef.freitas07@gmail.com>
2022-05-26 10:05:17 -0300

    chore: configuring prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,8 +4,20 @@ import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom'
 
-export default function Posts(){
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostProps){
   return(
     <>
       <Head>
@@ -14,21 +26,14 @@ export default function Posts(){
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de abril de 2022</time>
-            <strong>Creating a Monorepo with Learn & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
-          </a>
-          <a href="#">
-            <time>12 de abril de 2022</time>
-            <strong>Creating a Monorepo with Learn & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
-          </a>
-          <a href="#">
-            <time>12 de abril de 2022</time>
-            <strong>Creating a Monorepo with Learn & Yarn Workspace</strong>
-            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared</p>
-          </a>
+
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -45,9 +50,22 @@ export const getStaticProps: GetStaticProps = async () => {
    pageSize: 100,
  })
 
- console.log(JSON.stringify(response, null, 2));
+ const posts = response.results.map(post => {
+   return {
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    })
+   }
+ })
 
- return {
-    props: {}
- }
+  return {
+    props: {
+      posts
+    }
+  }
 }
